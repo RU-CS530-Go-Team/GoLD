99209f99db83be54375313f622aed74fc76bc04c
Zachary Daniels <zacharydaniels@nbp-109-74.nbp.ruw.rutgers.edu>
2014-11-23 21:53:33 -0500

    Started model building

--- src/gold/learn/trainer.py
+++ src/gold/learn/trainer.py
@@ -13,6 +13,24 @@ from gold.features.StoneCountFeature import StoneCountFeature
 from gold.features.DiffLiberties import DiffLiberties
 from gold.features.DistanceFromCenterFeature import DistanceFromCenterFeature
 from gold.features.ColorFeature import ColorFeature
+from sklearn import svm
+import numpy as np
+import pickle
+
+class ModelBuilder():
+  def __init__(self,inputFile):
+    self.trainingFile = inputFile
+    self.trainingData = np.loadtxt(open(self.trainingFile,"rb"),delimiter=",",skiprows=1)
+    self.instances = self.trainingData[:,:self.trainingData.shape[1]-1]
+    self.classes = self.trainingData[:,self.trainingData.shape[1]-1]
+
+  def buildModelSVM(self,outputFile):
+    clf = svm.LinearSVC()
+    clf.fit(self.instances, self.classes)
+    s = pickle.dumps(clf)
+    f = open(outputFile,"w")
+    f.write(s)
+    f.close()
 
 class FeatureExtractor():
 
