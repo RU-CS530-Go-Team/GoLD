313c700eaf8fea0196628cc5bdf3261e8eb448ee
MrTyton <joshua.gang@gmail.com>
2014-11-26 09:00:52 -0500

    Should work now
    I tested it on several test cases, they all seemed to work. This only
    test for UNCONDITIONAL life, not for groups that are for all intents and
    purposes alive (like the 4-in a row). If someone else wants to test it
    on more cases, feel free (or if there was some easy way to generate test
    cases: can we modify the gui so that I can just place moves on it and
    then have it output the live stones? I mean, the packaging doesn't work
    for me still, but I haven't really tried to fix it yet)
    
    If everyone agrees with this then we can merge it.

--- src/gold/extraneous/life.py
+++ src/gold/extraneous/life.py
@@ -1,3 +1,4 @@
+#from board import *
 from gold.models.board import StoneGrouper
 
 
@@ -8,20 +9,30 @@ def determineLife(board, color):
     else:
         stones = board.white_stones
         other = board.black_stones
-    initial_groups = StoneGrouper(stones)
-    connected_groups = StoneGrouper(stones, board)
+    initial_groups = [list(x) for x in StoneGrouper(stones).groups]
+    connected_groups = [list(x) for x in StoneGrouper(stones, board).groups]
+    #print initial_groups
+    #print connected_groups
     
     #Connect all of the empty squares (ignoring the other color)
-    fill_board = [[0] * board.y] * board.x
-    for (x, y) in stones:
+    fill_board = []
+    for i in range(board.x):
+        temp = []
+        for w in range(board.y):
+            temp.append(0)
+        fill_board.append(temp)
+    #fill_board = [[0] * board.y] * board.x
+    #print fill_board
+    for x, y in stones:
         fill_board[x][y] = 1
+    #print fill_board
     start_color = 2
     for x_i, x in enumerate(fill_board):
         for y_i, y in enumerate(x):
             if y == 0:
                 flood_fill(fill_board, (x_i, y_i), 0, start_color)
                 start_color += 1
-                
+    #print fill_board
     #Split up the filled groups into something that we can easily parse
     regions = {}
     for x_i, x in enumerate(fill_board):
@@ -31,20 +42,20 @@ def determineLife(board, color):
                 regions[y].append((x_i, y_i))
             except:
                 regions[y] = [(x_i, y_i)]
-                
+    #print regions
     #remove all places taken up by the other color of stones
     for current_region in regions:
         for check in other:
             if check in regions[current_region]:
-                regions[current_region].remove(other)
-                
+                regions[current_region].remove(check)
+    #print regions
     #Determine which are enclosed by whatever color we're working on
     enclosed_regions = []
     groups_by_region = {}
     for current_group in connected_groups:
         max_X = max([x[0] for x in current_group]) +1
         max_Y = max([x[1] for x in current_group]) +1
-        min_Y = min([x[0] for x in current_group]) -1
+        min_X = min([x[0] for x in current_group]) -1
         min_Y = min([x[1] for x in current_group]) -1
         
         for current_region in regions:
@@ -54,23 +65,30 @@ def determineLife(board, color):
                     groups_by_region[current_region].append(current_group)
                 except:
                     groups_by_region[current_region] = [current_group]
-        
+    #print enclosed_regions
+    #print groups_by_region
     #Starting Benson's Algorithm
     initial_groups = sorted(initial_groups)
     while(True):
         modified = False
         for chain in initial_groups:
+            #print chain
             num_vital = 0
             for region in enclosed_regions:
+                #print region
                 if vital(chain, board, region):
                     num_vital += 1
+                    #print "Is Vital"
+                    #print chain, region
             if num_vital < 2:
+                #print "Removing due to lack of vital areas"
+                #print chain
                 initial_groups.remove(chain)
                 modified = True
         if not modified:
             break
         modified = False
-        for region in groups_by_region:
+        for region in groups_by_region.keys():
             cmodified = False    
             current_groups = groups_by_region[region]
             for each_group in current_groups:
@@ -78,12 +96,20 @@ def determineLife(board, color):
                     if any(each_stone not in looking_at_group for looking_at_group in initial_groups):
                         modified = True
                         cmodified = True
+                        #print "Getting rid of something"
+                        #print region
+                        #print groups_by_region
+                        #print regions[region]
+                        #print groups_by_region[region]
                         del groups_by_region[region]
-                        enclosed_regions.remove(region)
+                        #print enclosed_regions
+                        enclosed_regions.remove(regions[region])
                         break
                 if cmodified: break
         if not modified: break
                 
+                
+    #print initial_groups
     return initial_groups
     
     
@@ -103,8 +129,12 @@ def flood_fill(fill_board, current_node, target_color, replacement_color):
     return
 
 def vital(chain, board, region):
-    liberties = calculate_liberties(board, chain)
-    return sorted(liberties) == sorted(region)
+    
+    liberties = calculate_liberties(chain, board)
+    #print "In Vital"
+    #print liberties
+    #print region
+    return all(x in liberties for x in region)
 
 
 def calculate_liberties(group, board):
@@ -114,7 +144,12 @@ def calculate_liberties(group, board):
             for cm in moves:
                 if cm[0] < 0 or cm[0] >= board.x or cm[1] < 0 or cm[1] >= board.y:
                     continue
-                elif not (cm in boardblack_stones or cm in board.white_stones):
+                elif not (cm in board.black_stones or cm in board.white_stones):
                     current_liberties.add(cm)
         return list(current_liberties)
-        
+    
+#test = Board(3, 5)
+#test.black_stones = [(1, 0), (1, 1), (1, 2), (0, 1), (2, 4), (2, 3), (2, 2)]
+#test.white_stones = [(1, 3)]
+#print test
+#determineLife(test, True)      
