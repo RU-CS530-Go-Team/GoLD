090a139160940ece8ca7d371288e5b4f7d49bc89
John Blackmore <john.blackmore@rutgers.edu>
2014-12-06 16:45:32 -0500

    Added convert_csv_for_terminal_test

--- src/gold/features/extractor.py
+++ src/gold/features/extractor.py
@@ -7,10 +7,11 @@ from numpy import ndarray, float64
 from types import ListType, IntType, FloatType, StringType
 import csv
 from gold.features import *
+'''
 FEATURE_SERVICES = ['ColorFeature', 'StoneCountFeature', 'DiffLiberties', 'DistanceFromCenterFeature', 
                     'DistanceFromCenterFeature', 'numberLiveGroups', 'HuMomentsFeature',
                     'LocalShapesFeature', 'SparseDictionaryFeature']
-
+'''
 class FeatureExtractor():
 
     def __init__(self):
@@ -19,8 +20,8 @@ class FeatureExtractor():
     def init_feature_services(self, start, move, movePosition, isblack):
         feature_services = dict()
         globals_hash = globals()
-        
-        services = [s for s in FEATURE_SERVICES if s!='PatchExtractor']
+        services = globals()['FEATURE_SERVICES']
+        #services = [s for s in FEATURE_SERVICES if s!='PatchExtractor']
         for service in services:
             feature_services[service] = globals_hash[service](start, move, movePosition, isblack)
             if service=='PatchExtractor':
@@ -66,6 +67,45 @@ class FeatureExtractor():
             else:
                 raise Exception('{}: Unexpected type {}'.format(fk,fvtype))
         return features
+
+    def convert_csv_for_terminal_test(self, csvfname, probtyp, newcsvname=None):
+        descArr = ['_', 'BtL', 'WtK']
+        probDesc = descArr[probtyp]
+        if newcsvname==None:
+            newcsvname = csvfname+'.t'+probDesc
+        with open(csvfname, 'r') as csvfile, open(newcsvname,'w') as csvout:
+            rdr = csv.DictReader(csvfile)
+      
+            headers = FeatureExtractor().sort_headers(rdr.fieldnames)
+            wtr = csv.DictWriter(csvout, fieldnames=headers+['OUTCOME'], lineterminator='\n')
+            wtr.writeheader()
+            for row in rdr:
+                #print('{}.{}.{}'.format(row['_DI'],row['_PROBID'],row['_MOVE']))
+                pt = row['_PT']
+                if pt==str(probtyp):
+                    # Black-to-live; move for black
+                    if probtyp==1:
+                        if row['ColorFeature']=='1':
+                            outcome = int(row['_SOLUTION'])*int(row['_TERM']) 
+                        else:
+                            outcome = '0'
+                    # White-to-kill
+                    elif probtyp==2:
+                        if row['ColorFeature']=='0':
+                            outcome = int(row['_SOLUTION'])*int(row['_TERM']) 
+                        else:
+                            outcome = '0'
+                    # ?-to-? 
+                    else:
+                        outcome = int(row['_SOLUTION'])*int(row['_TERM'])
+
+                    newrow = {h: float(row[h]) for h in headers}
+                    if 'OUTCOME' not in headers:
+                        newrow['OUTCOME']= outcome
+                    elif(outcome != row['OUTCOME']):
+                        raise ValueError('{}.{}.{}: Outcomes don''t match.'.format(row['_DI'],row['_PROBID'],row['_MOVE']))
+                    wtr.writerow(newrow)
+        return newcsvname    
     
     def convert_csv_for_probtype(self, csvfname, probtyp):
         newcsvname = csvfname+'.'+str(probtyp)
@@ -102,3 +142,17 @@ class FeatureExtractor():
                         raise ValueError('{}.{}.{}: Outcomes don''t match.'.format(row['_DI'],row['_PROBID'],row['_MOVE']))
                     wtr.writerow(newrow)
         return newcsvname
+'''
+csvfname = 'c:/users/jblackmore/documents/development/rutgers/gold/problems/resplit/trainfeatures.csv'
+newcsvname = 'c:/users/jblackmore/documents/development/rutgers/gold//problems/resplit/trainfeaturesBtL_T.csv'
+FeatureExtractor().convert_csv_for_terminal_test(csvfname, 1, newcsvname)
+csvfname = 'c:/users/jblackmore/documents/development/rutgers/gold//problems/resplit/trainfeatures.csv'
+newcsvname = 'c:/users/jblackmore/documents/development/rutgers/gold//problems/resplit/trainfeaturesWtK_T.csv'
+FeatureExtractor().convert_csv_for_terminal_test(csvfname, 2, newcsvname)
+'''
+csvfname = 'c:/users/jblackmore/documents/development/rutgers/gold/problems/resplit/devfeatures.csv'
+newcsvname = 'c:/users/jblackmore/documents/development/rutgers/gold/problems/resplit/devfeaturesBtL_T.csv'
+FeatureExtractor().convert_csv_for_terminal_test(csvfname, 1, newcsvname)
+csvfname = 'c:/users/jblackmore/documents/development/rutgers/gold//problems/resplit/devfeatures.csv'
+newcsvname = 'c:/users/jblackmore/documents/development/rutgers/gold//problems/resplit/devfeaturesWtK_T.csv'
+FeatureExtractor().convert_csv_for_terminal_test(csvfname, 2, newcsvname)
