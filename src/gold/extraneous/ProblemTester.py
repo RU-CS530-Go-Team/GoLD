9843c8ef854019b2c054263320e968a3a309754d
John Blackmore <john.blackmore@rutgers.edu>
2014-12-08 11:05:06 -0500

    ProblemTester is good to go
    Look at main, modify or create your own. Key function is
    test_problems(), writes results to outputfile if provided

--- src/gold/extraneous/ProblemTester.py
+++ src/gold/extraneous/ProblemTester.py
@@ -5,19 +5,21 @@ Created on Nov 28, 2014
 '''
 from gold.extraneous.MoveTreeParser import MoveTreeParser,\
     UnspecifiedProblemType
-from gold.models.board import Board, IllegalMove
+from gold.models.board import IllegalMove
 from random import seed, shuffle
-import csv
 import sys
 import os
-from gold.learn.trainer import FeatureExtractor
 from gold.models.search import MinMaxTree
-from gold.ui.Launcher import Launcher
 from gold.extraneous.life import determineLife
-from gold.extraneous.terminalLife import findAliveGroups
 from gold.learn.Model import Model
 from glob import glob
 
+class YouLoseException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+    
 def print_move(label, move):
     liveWGr = determineLife(move, False)
     liveBGr = determineLife(move, True)
@@ -28,46 +30,6 @@ def print_move(label, move):
     wa = 0#len(aliveWGr)
     wb = 0#len(aliveBGr)
     print('{}: w={}, b={}, wa={}, wb={}, nw={}, nb={}'.format(label,w,b,wa,wb,len(move.white_stones),len(move.black_stones)))
-
-''' Checks problem type and number of living groups
-    against terminal states from the problem. 
-'''
-def term_test(probfile, testResults=dict()):
-    mtp = MoveTreeParser(probfile)
-    ss = mtp.getAllPaths()
-    move = mtp.start.clone()
-    #print_move('start ({}x{})'.format(move.x, move.y), move)
-    isBtL = (mtp.problemType == 1 or mtp.problemType==3)
-    pt = 1 if isBtL else 2
-    testResults['PT'] = pt
-    sw = len(determineLife(move, False))
-    sb = len(determineLife(move, True))
-    if sb>0:
-        print('{}: Black is already alive!'.format(probfile))
-    # prob type, solution, terminal, # living black groups, # living white groups
-    #tpl= (pt, 1, 0, sb, sw)
-    tpl= (pt, 1, 0, 0, 0)
-    if tpl in testResults.keys():
-        testResults[tpl] = testResults[tpl]+1
-    else:
-        testResults[tpl]=1
-    for spath in ss:
-        move = mtp.start.clone()
-        for step in spath:
-            fm = mtp.formatMove(mtp.moveID[step])
-            move.place_stone(fm['x'], fm['y'], fm['isBlack'])
-            w = len(determineLife(move, False))
-            b = len(determineLife(move, True))
-            sol = 1 if step in mtp.getSolutionNodes() else 0
-            term = 1 if step in mtp.getTerminalStates() else 0
-            bdiff = b-sb
-            wdiff = w-sw    
-            tpl = (pt, sol, term, b, w)
-            if tpl in testResults.keys():
-                testResults[tpl] = testResults[tpl]+1
-            else:
-                testResults[tpl]=1
-    return testResults
 
 def test_problem(probfile, modelBtL, modelWtK):
     print(probfile)
@@ -81,6 +43,8 @@ def test_problem(probfile, modelBtL, modelWtK):
     longestPath = 0
     isBtL = (mtp.problemType == 1 or mtp.problemType==3)
     print(mtp.getProblemTypeDesc()+' - black={}, isBtL={}'.format(isblack, isBtL))
+    sb = len(determineLife(mtp.start, True))
+    print('SOLUTIONS:')
     for spath in ss:
         pathLength = 0
         move = mtp.start.clone()
@@ -115,25 +79,65 @@ def test_problem(probfile, modelBtL, modelWtK):
         move.place_stone(mmt.i, mmt.j, isblack)
         #b = len(determineLife(move,True))
         print_move('{}({},{})'.format(color,mmt.i, mmt.j), move)
-        mmt.promote()
         if move in terminalIncorrectStates:
-            raise 'Haha! You lose!'
+            raise YouLoseException('Haha! You lose!')
         if move in solutionStates:
             print('{}: You Win!!'.format(probfile))
             return
+        b = len(determineLife(move, True))
+        if b>sb and isBtL:
+            print('You Win!!! Black has {} groups that are unconditionally alive!'.format(b))
+            return
         pathLength = pathLength+1
-        #w = len(deter(move,False))
-        #b = len(findAliveGroups(move, True))
-        #print('{}({},{}): w={}, b={}, nw={}, nb={}'.format(color, nextMove.i, nextMove.j,w,b,len(move.white_stones),len(move.black_stones)))
+        if pathLength>=2*longestPath:
+            raise YouLoseException('Too many moves, you lose!!')
+            
         isblack = not isblack
-    raise Exception('Too many moves, you lose!!')
+        mmt.promote()
     '''
     ui=Launcher(400,400,50,max(move.x,move.y))
     ui.setBoard(move)
     ui.drawBoard()
     ui.mainloop()
     '''
-    
+def parse_problem_filename(probfile):
+    subpaths = probfile.split('/')
+    if subpaths[-1].rfind('\\')>0:
+        subsub = subpaths[-1].split('\\')
+        problemId = subsub[-1]
+        difficulty = subsub[-2]
+    else:
+        difficulty= probfile.split('/')[-2]
+        problemId = probfile.split('/')[-1]
+    problemId = problemId.split('.')[-2]
+    return [problemId, difficulty]
+
+def call_test_problem(probfile, modelBtL, modelWtK, fout=None):
+    ''' Returns 1 if problem is solved
+        Returns 0 if problem could not be solved
+        Returns -1 if problem could not be executed
+    '''
+    if probfile[-3:]=='sgf':
+        [problemId, difficulty]=parse_problem_filename(probfile)
+        #print(probdiff)
+        try:
+            test_problem(probfile, modelBtL, modelWtK)
+            if fout is not None:
+                fout.write('{},{},1\n'.format(problemId, difficulty))
+            return 1
+        except UnspecifiedProblemType:
+            return -1
+        except IllegalMove as im:
+            print(im)
+            return -1
+        except YouLoseException as yle:
+            print(yle)
+            if fout is not None:
+                fout.write('{},{},0\n'.format(problemId, difficulty))
+            return 0
+    else:
+        raise Exception('{} is not a .sgf file'.format(probfile))
+            
 def been_here_before(priorStates, currentState):
     for prior in priorStates:
         whiteSet = prior[0]
@@ -154,86 +158,79 @@ def load_model(modelFile, modelType, scalerFile):
     model.setScaler(scalerFile)
     print('Model and scaler files ready!')
     return model
+
+def test_problems(modelBtl, modelWtK, probdirs, outputfile):
+    
+    #test_problem(sys.argv[1], modelBtL, modelWtK)
+    with open(outputfile, 'w') as fout:
+        fout.write('PROBLEM,DIFFICULTY,SCORE\n')
+        seed(1234567890)
+        totalNumCorrect = 0
+        totalTotal = 0
+        for problemDir in probdirs:
+            print problemDir
+            numCorrect = 0
+            numTotal = 0
+            if not os.path.isdir(problemDir):
+                if len(problemDir)<3:
+                    print('Not a dir or problem file: {}'.format(problemDir))
+                elif problemDir[-3]=='sgf':
+                    result = call_test_problem(problemDir, modelBtL, modelWtK, fout)
+                    if result>=0:
+                        numTotal+=1
+                        if result>0:
+                            numCorrect+=1
+
+                else:
+                    print('Not a dir or problem file: {}'.format(problemDir))
+                sys.exit()
+            dirs = glob(problemDir+'/*')
+            shuffle(dirs)
+            for probdiff in dirs:
+                if os.path.isdir(probdiff):
+                    files = glob(probdiff+'/*.sgf')
+                    for probfile in files:
+                        
+                        result = call_test_problem(probfile, modelBtL, modelWtK, fout)
+                        if result>=0:
+                            numTotal+=1
+                            if result>0:
+                                numCorrect+=1
+                        
+                else:
+                    if probdiff[-3:]=='sgf':
+                        result = call_test_problem(probdiff, modelBtL, modelWtK, fout)
+                        if result>=0:
+                            numTotal+=1
+                            if result>0:
+                                numCorrect+=1
+                if result>=0:
+                    print('{}/{} correct'.format(numCorrect, numTotal))
+
+            totalNumCorrect+=numCorrect
+            totalTotal+=numTotal
+            print('{}/{} correct'.format(numCorrect, numTotal))
+        print('{}/{} correct'.format(totalNumCorrect, totalTotal))
+    
+
 if __name__ == '__main__':
     if len(sys.argv)<3:
-        print('Usage: python ProblemTester <model_dir> <problem_dir>')
+        print('Usage: python ProblemTester <model_dir> <problem_dir_or_file1> [problem_dir_or_file2...]')
         sys.exit()
+    
+    # Sample main... make your own if you want something different
+    # Just import load_model and test_problems
     modelDir = sys.argv[1]
-    problemDirs = sys.argv[2:]
-    print(problemDirs)
-    modelFile = modelDir+'/modelRF100BtL.txt'
+    modelFile = modelDir+'/modelNBBtL.txt'
     modelType = 3 
     scalerFile = modelDir+'/trainfeaturesBtLScaler.txt'
     modelBtL = load_model(modelFile, modelType, scalerFile)
-    modelFile = modelDir+'/modelRF100WtK.txt'
+    modelFile = modelDir+'/modelNBWtK.txt'
     modelType = 3
     scalerFile = modelDir+'/trainfeaturesWtKScaler.txt'
     modelWtK = load_model(modelFile, modelType, scalerFile)
-    #test_problem(sys.argv[1], modelBtL, modelWtK)
+    problemDirs = sys.argv[2:]
 
-    terms = dict()
-    seed(1234567890)
-    totalNumCorrect = 0
-    totalTotal = 0
-    for problemDir in problemDirs:
-        numCorrect = 0
-        numTotal = 0
-        if not os.path.isdir(problemDir):
-            if len(problemDir)<3:
-                print('Not a dir or problem file: {}'.format(problemDir))
-            elif problemDir[-3]=='sgf':
-                test_problem(problemDir, modelBtL, modelWtK)
-                #terms = term_test(problemDir, terms)
-            else:
-                print('Not a dir or problem file: {}'.format(problemDir))
-            sys.exit()
-        dirs = glob(problemDir+'/*')
-        shuffle(dirs)
-        for probdiff in dirs:
-            if os.path.isdir(probdiff):
-                files = glob(probdiff+'/*.sgf')
-                for probfile in files:
-                    
-                    print(probfile)
-                    try:
-                        #terms = term_test(probfile, terms)
-                        test_problem(probfile, modelBtL, modelWtK)
-                        numCorrect += 1
-                        numTotal += 1
-                    except UnspecifiedProblemType:
-                        x= 'UPT'
-                    except IllegalMove as im:
-                        print(im)
-                    except Exception as e:
-                        numTotal += 1
-                        print(e)
-            else:
-                if probdiff[-3:]=='sgf':
-                    try:
-                        #terms = term_test(probfile, terms)
-                        test_problem(probdiff, modelBtL, modelWtK)
-                        numCorrect += 1
-                        numTotal += 1
-                    except UnspecifiedProblemType:
-                        x= 'UPT'
-                    except IllegalMove as im:
-                        print(im)
-                    except Exception as e:
-                        print(e)
-                        x = 'WHATEVER'
-        totalNumCorrect+=numCorrect
-        totalTotal+=numTotal
-        print('{}/{} correct'.format(numCorrect, numTotal))
-    print('{}/{} correct'.format(totalNumCorrect, totalTotal))
-'''
-    for tpl in terms.keys():
-        try:
-            [pt,sol,term,nb,nw] = tpl
-            print('pt={},sol={},term={},nb={},nw={},count={}'.format(pt,sol,term,nb,nw,terms[tpl]))
-        except ValueError:
-            x = 'VER'
-'''
-    #test_problem('C:/Users/jblackmore/Documents/Development/Rutgers/GoLD/problems/resplit/train/2k/17324.sgf', modelBtL, None)
-    #test_problem('C:/Users/jblackmore/Documents/Development/Rutgers/GoLD/problems/resplit/train/25k/3052.sgf')
-    #test_problem('C:/Users/jblackmore/Documents/Development/Rutgers/GoLD/problems/resplit/train/25k/8177.sgf')
+    outputfile = modelDir+'/problem-test-results.txt'
+    test_problems(modelBtL, modelWtK, problemDirs, outputfile)
     
